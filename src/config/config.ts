ef944f46883f41d92fc21b744b4483d1d9dd6e0e
just-anohaker <just.anohaker@gmail.com>
2019-08-28 14:24:46 +0800

    commit project

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 export const SchedInterval: number = 30 * 60 * 1000;      // interval for sched (5s)
 
-const RootDir: string = "/root/anohaker/etm/dapps";
+const RootDir: string = "/root/anohaker/etm";
 const DiceDappId = "5c611b50ac47e136ffc10926895fffec7c32f25d32c2692abb471e250caaebf0";
-export const DiceDapp: string = RootDir + "/" + DiceDappId;
+export const DiceDapp: string = RootDir + "/dapps/" + DiceDappId;
 const SugramDappId = "f304169790def442a0f0451347f13b6aadbe12305da59fc2a7c2d81cb94cb27a"
-export const SugramDapp: string = RootDir + "/" + SugramDappId;
+export const SugramDapp: string = RootDir + "/dapps/" + SugramDappId;
